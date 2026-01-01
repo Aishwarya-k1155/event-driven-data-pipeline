ec70217f7e44462641fc73f18c570e8c5b21e412
Aishwarya-K1155 <kopulwaraishwarya88@gmail.com>
2026-01-01 19:30:42 +0530

    Fix S3 bucket conflict using random suffix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,15 @@ provider "aws" {
   region = var.region
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "raw_data" {
-  bucket = "${var.project_name}-raw-data-${var.unique_suffix}"
+  bucket = "${var.project_name}-raw-data-${random_id.suffix.hex}"
 }
 
 resource "aws_s3_bucket" "processed_data" {
-  bucket = "${var.project_name}-processed-data-${var.unique_suffix}"
+  bucket = "${var.project_name}-processed-data-${random_id.suffix.hex}"
 }
+
