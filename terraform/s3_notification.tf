4a3dd7b4dc1e9d00d7c37fe2209d3aa4d17a7dd9
Aishwarya-K1155 <kopulwaraishwarya88@gmail.com>
2026-01-01 20:52:00 +0530

    Fix Lambda zip path and IAM role conflict

--- terraform/s3_notification.tf
+++ terraform/s3_notification.tf
@@ -1,32 +1,44 @@
 ############################
-# USE EXISTING IAM ROLE
+# IAM ROLE FOR LAMBDA
 ############################
-data "aws_iam_role" "lambda_role" {
-  name = "event-driven-data-pipeline-lambda-role"
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 ############################
 # LAMBDA FUNCTION
 ############################
 resource "aws_lambda_function" "s3_processor" {
-  function_name = "s3-raw-to-processed"
-  role          = data.aws_iam_role.lambda_role.arn
+  function_name = "${var.project_name}-s3-processor"
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
 
-  filename = "lambda.zip"
-
-  depends_on = [
-    aws_s3_bucket.raw_data,
-    aws_s3_bucket.processed_data
-  ]
+  filename         = "../lambda/lambda_function.zip"
+  source_code_hash = filebase64sha256("../lambda/lambda_function.zip")
 }
 
 ############################
 # ALLOW S3 TO INVOKE LAMBDA
 ############################
 resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "AllowExecutionFromS3"
+  statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.s3_processor.function_name
   principal     = "s3.amazonaws.com"
@@ -34,9 +46,9 @@ resource "aws_lambda_permission" "allow_s3" {
 }
 
 ############################
-# S3 EVENT NOTIFICATION
+# S3 NOTIFICATION TRIGGER
 ############################
-resource "aws_s3_bucket_notification" "raw_trigger" {
+resource "aws_s3_bucket_notification" "raw_bucket_notification" {
   bucket = aws_s3_bucket.raw_data.id
 
   lambda_function {
@@ -44,7 +56,5 @@ resource "aws_s3_bucket_notification" "raw_trigger" {
     events              = ["s3:ObjectCreated:*"]
   }
 
-  depends_on = [
-    aws_lambda_permission.allow_s3
-  ]
+  depends_on = [aws_lambda_permission.allow_s3]
 }
